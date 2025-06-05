ff16d6a2b9326c88df1060e7da2c160e2033a803
shamil <shamilmohd418@gmail.com>
2025-06-05 11:48:53 +0530

    working on blogs page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AllProducts from './Component/pages/AllProducts/AllProducts'
 import ExploreProducts from './Component/pages/ExploreProducts/ExploreProducts'
 import AboutUs from './Component/pages/AboutUs/AboutUs'
 import ProductPage from './Component/pages/ProductPage/ProductPage'
+import Blogs from './Component/pages/Blogs/Blogs'
 
 function App() {
 
@@ -23,6 +24,8 @@ function App() {
           <Route path='/ExploreProducts' element={<ExploreProducts />} />
           <Route path='/aboutus' element={<AboutUs />} />
           <Route path='/Product-page' element={<ProductPage />} />
+          <Route path='/blogs' element={<Blogs />} />
+          {/* <Route path='/contact-us' element={<ContactUs />} /> */}
         </Routes>
 
 
---
+++ src/Component/pages/Blogs/Blogs.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../../common/Navbar/Navbar'
+import './Blogs.scss'
+const Blogs = () => {
+  return (
+    <div className='BlogsMainWrapper'>
+      <Navbar />
+      <div className='blog-header'>
+            <h1>Our Blogs</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Blogs
